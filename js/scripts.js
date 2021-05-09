46b3e6632d63bb071b20bba287ea4c23317107a2
Jim Medios <49573733+jmedios@users.noreply.github.com>
2021-05-09 11:50:57 -0700

    feat: add css and jQuery refactor

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+function newItem() {
+  let li = $("<li></li>");
+  let inputValue = $("#input").val();
+  li.append(inputValue);
+
+  if (inputValue === "") {
+    alert("Please write something!");
+  } else {
+    $("#list").append(li);
+  }
+
+  // To Cross Out Items
+  function crossOut() {
+    li.toggleClass("strike");
+  }
+
+  li.on("dblclick", function crossOut() {
+    li.toggleClass("strike");
+  });
+
+  // Add delete button
+  let crossOutButton = $("<crossOutButton></crossOutButton>");
+  crossOutButton.append(document.createTextNode("X"));
+  li.append(crossOutButton);
+
+  crossOutButton.on("click", deleteListItem);
+  function deleteListItem() {
+    li.addClass("delete");
+  }
+  //Make items sortable
+  $("#list").sortable();
+}
